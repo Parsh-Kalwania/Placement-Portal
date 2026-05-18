ad2f92f7bc8bdce7d7bb6fda130ca5566b5c7409
Parsh Kalwania <satyarajkalmaniya1978@gmail.com>
2026-05-19 00:58:58 +0530

    fix: wrap wsgi.py in a diagnostic block to print startup tracebacks to Render logs

--- placement_portal/wsgi.py
+++ placement_portal/wsgi.py
@@ -8,14 +8,22 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import traceback
+
 try:
     from dotenv import load_dotenv
     load_dotenv()
 except ImportError:
     pass
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'placement_portal.settings')
 
-application = get_wsgi_application()
+try:
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
+except Exception as e:
+    sys.stderr.write("!!! CRITICAL: Django WSGI initialization failed !!!\n")
+    traceback.print_exc(file=sys.stderr)
+    sys.stderr.flush()
+    raise e
